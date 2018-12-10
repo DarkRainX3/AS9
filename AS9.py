232125f8c392661cf21ea5d342bec8dbe151eb12
Waley <44916399+DarkRainX3@users.noreply.github.com>
2018-12-09 23:35:52 -0700

    done

--- AS9.py
+++ AS9.py
@@ -23,22 +23,24 @@ from sklearn.neighbors import KNeighborsRegressor
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
 
 scores = list()
 nasa = pd.read_csv('NasaData.csv')
-x = nasa.iloc[:,0:-1]
-y = nasa.iloc[:,-1]
+x = nasa.iloc[:, 0:-1]
+y = nasa.iloc[:, -1]
 
-for i in range(1,31):
-    xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size = 0.25, random_state=i)#the test size is the % of data used to test the model, the rest of the data
-    #is used to train the model
-    knn= KNeighborsClassifier(n_neighbors=5)
-    knn.fit(xtrain,ytrain)
-    scores.append(knn.score(xtest,ytest))
+for i in range(1, 31):
+    # the test size is the % of data used to test the model, the rest of the data
+    xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.25, random_state=i)
+    # is used to train the model
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(xtrain, ytrain)
+    scores.append(knn.score(xtest, ytest))
 scores
-plot = pd.DataFrame({"KNN":scores})
-axes = plot.boxplot(column=['KNN'],return_type="axes")
-axes.set_ylim([0,1])
+plot = pd.DataFrame({"KNN": scores})
+axes = plot.boxplot(column=['KNN'], return_type="axes")
+# axes.set_ylim([0,1])
 # print("Test Score: {:.3f}".format(knn.score(xtest,ytest)))
 
 
@@ -48,31 +50,34 @@ spot that the model is neither overfitted nor underfitted. Here again take the N
 model_selection's train_test_split with 75% training and 25% test data, but with a fix random_state=42.
 Then build a K-Nearest-Neighbors model using k=1,3,5,..,49. Finally, plot the accuracy of your models on
 the training dataset and the testing dataset, using two lines in one plot.
-
 Using this plot identify what the best value is for k."""
 
 nasa = pd.read_csv('NasaData.csv')
-x = nasa.iloc[:,0:-1]
-y = nasa.iloc[:,-1]
-xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size = 0.25, random_state=42)
-trainacc=list()
-testacc= list()
-DifferentK=[i*2-1 for i in range(1,25)]
+x = nasa.iloc[:, 0:-1]
+y = nasa.iloc[:, -1]
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.25, random_state=42)
+trainacc = list()
+testacc = list()
+DifferentK = [i*2-1 for i in range(1, 25)]
 for k in DifferentK:
-    knn= KNeighborsClassifier(n_neighbors=k)
-    knn.fit(xtrain,ytrain)
-    testacc.append(knn.score(xtest,ytest))
-    trainacc.append(knn.score(xtrain,ytrain))
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(xtrain, ytrain)
+    testacc.append(knn.score(xtest, ytest))
+    trainacc.append(knn.score(xtrain, ytrain))
 
-def PlotDrawer(Plot_one,Plot_Two,X_axis):
-    plt.plot(X_axis,Plot_one,label="training accuracy")
-    plt.plot(X_axis,Plot_Two,label="test accuracy")
+
+def PlotDrawer(Plot_one, Plot_Two, X_axis):
+    plt.plot(X_axis, Plot_one, label="training accuracy")
+    plt.plot(X_axis, Plot_Two, label="test accuracy")
     plt.ylabel('Mean Accuracy')
     plt.xlabel('K-value')
     plt.legend()
     plt.show()
-PlotDrawer(trainacc,testacc,DifferentK)#right is underfit (train and test acc are close(higher k)) left is overfit (train and test acc are very diff(lower k))
-#best value for k is around 15
+
+
+# right is underfit (train and test acc are close(higher k)) left is overfit (train and test acc are very diff(lower k))
+PlotDrawer(trainacc, testacc, DifferentK)
+# best value for k is around 15
 
 """Part C. Regression
 In this section, we will use a new data set which is related to the performance of several CPUs.
@@ -100,4 +105,10 @@ To see how good you can predict on new CPUs performance, print the score of the 
 Use the default setup for the models."""
 
 data = pd.read_csv("CPU_Performance.csv")
-data
+x = data.iloc[:, 0:-1]
+y = data.iloc[:, -1]
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.25, random_state=42)
+knr = KNeighborsRegressor(n_neighbors=5)
+knr.fit(xtrain, ytrain)
+print(knr.score(xtrain, ytrain))
+print(knr.score(xtest, ytest))
